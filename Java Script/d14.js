ac045a3a6e516ab00ceb902b01de98a3a0ca0190
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-17 17:18:29 +0530

    Day 14 js start :)

---
+++ Java Script/d14.js
@@ -0,0 +1,34 @@
+// const accountID =14553
+// let accountEmail = "chirag@gmail.com"
+// var accountPass ="12345"
+// accountCity ="Jaipur"
+// let accou
+// // accountID =2 
+
+// accountEmail="Ck@gmail.com"
+// accountPass ="2121"
+// accountCity= "Haryana"
+// console.log(accountID);
+// // 
+// console.table([accountID,accountEmail,accountCity,accountPass])
+
+"use strict";
+// alert(3+3) //we are using node js note browser
+
+let name ="chirag"
+let age =18
+let isLogged =false
+
+/*
+    number=> 2 to power 53
+    null=>standlone value
+    undefined=>
+    string=>""
+    symbol=> unique
+    bigInt
+
+    object:
+*/
+console.log(typeof "chirag");
+console.log(typeof "null");
+
