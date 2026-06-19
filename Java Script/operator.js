895240bae81ffa5addce1ac988db65c1c3dc81e6
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-19 15:59:33 +0530

    Operator done

--- Java Script/operator.js
+++ Java Script/operator.js
@@ -11,3 +11,20 @@ console.log(a**3); */ // Power
 // NaN =Not a number 0/0 = Nan;
 /*console.log(typeof NaN);
 console.log( NaN +1); */
+
+// a =a+1 //a+=1; Assignment Operator
+// console.log(a);
+
+//Uranary Operator:
+// let c=a + ++b;
+// let d = a +b++;
+// console.log(c);
+// console.log(d);
+
+//String Indices:
+// let name="Chirag Rewaliya"
+// console.log(name);
+// console.log(name[0]);
+// console.log(name[100]);
+// console.log(name.length);
+
