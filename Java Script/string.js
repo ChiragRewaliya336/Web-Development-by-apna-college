5e8f0ba6de4a6e6fc3935e42999a33a468776fc3
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-22 17:22:13 +0530

    Day18 Strings Method

---
+++ Java Script/string.js
@@ -0,0 +1,28 @@
+// // stringName.method()
+let msg ="Hello";
+// console.log(msg);
+// //  let password =prompt("Set your password: ");
+// let newPass = password.trim();
+//  console.log(password); // original
+// console.log(newPass);  // trimmed
+
+// changing case:
+// let name = "Chirag";
+// console.log(name);
+// console.log(name.toUpperCase());
+
+// Indexof and multiiple method in one line:
+// console.log(name.indexOf("ir"));
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+// Slice method
+// console.log(msg.slice(0,5));
+// console.log(msg.slice(-5));
+
+// Replace and repeat method:
+console.log(msg.replace("ello","ii"));
+let name = "Chirag Chirag ";
+// console.log(name.replace("Chirag","Rewaliya "));
+// console.log(name.repeat(2));
+
