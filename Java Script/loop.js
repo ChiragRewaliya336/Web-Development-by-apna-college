8d0da7a3be8833f00bb50cdd6bce80ea33c62d30
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-25 12:31:11 +0530

    Loop statemnts

---
+++ Java Script/loop.js
@@ -0,0 +1,93 @@
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+
+// for(let i =1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Odd Numbers:
+// for(let i=0; i<=16; i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+// console.log("Even Numbers: ");
+// for(let i=1; i<=16; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i=5; i<=50; i=i+5){
+//     console.log(i);
+// }
+
+// for(let i=1; i<=5; i++){
+//     console.log(`Outer loop ${i}`);
+//     for(let j=1; j<=5; j++){
+//         console.log(j); 
+//     }
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++
+// }
+
+// favourite movie:
+// let guess = prompt("Guess my favourite movie");
+// let favmov ="Bhaag Milkha Bhaag";
+// while(guess!=favmov && guess!="quit"){
+//     guess = prompt("Wrong Guess.Please Try again: ");
+// }
+// if(guess==favmov){
+//     console.log('Congratulation');
+// }
+// else{
+//     console.log('Kr le bhai yrr');
+    
+// }
+
+// Break Keyword:
+// let i=1;
+// while(i<=7){
+//     if(i==6){
+//         break;
+//     }
+//     if(i==4){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// Loop with array:
+// let fruit =["mango","apple","bnana","litchi","orange"];
+// for(let i=0; i<fruit.length; i++){
+//     console.log(i, fruit[i]);
+// }
+
+// Nested Loop with array:
+// let heroes = [["IronMan","SpiderMan","Thor"],["SuperMan","Wonder woman","Flash"]];
+// for(let i=0; i<heroes.length; i++){
+//     console.log(`List: ${i}`);
+//     for(j=0; j<heroes[i].length; j++){
+//         console.log(j, heroes[i][j] );
+//     }   
+// }
+
+// Nested for of loop:
+// for(list of heroes){
+//    for(hero  of list){
+//     console.log(hero);
+//    }  
+// }
+
+
+
